2cf8021d408af7ff3edf601954aea6dd46110b8f
Novonil Das <das.novonil@gmail.com>
2021-01-09 15:46:53 -0600

    Binary Search

---
+++ Leetcode/Binary Search/FindSmallestLetterGreaterThanTarget.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode.Binary_Search
+{
+	class FindSmallestLetterGreaterThanTarget
+	{
+		public static char NextGreatestLetter(char[] letters, char target)
+		{
+			int start = 0;
+			int end = letters.Length - 1;
+			int mid;
+			int n = letters.Length;
+
+			while(start<=end)
+			{
+				mid = start + (end - start) / 2;
+				if(letters[mid] <= target)
+				{
+					start = mid + 1;
+				}
+				else
+				{
+					end = mid - 1;
+				}
+			}
+			return letters[start % n];
+		}
+	}
+}
