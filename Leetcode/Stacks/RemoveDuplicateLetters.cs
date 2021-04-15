2676a9dcd5868a002f71c268f72ce705ac61b8df
Novonil Das <das.novonil@gmail.com>
2021-04-15 11:05:56 -0500

    Updated

--- Leetcode/Stacks/RemoveDuplicateLetters.cs
+++ Leetcode/Stacks/RemoveDuplicateLetters.cs
@@ -6,5 +6,46 @@ namespace Leetcode.Stacks
 {
 	class RemoveDuplicateLetters
 	{
-	}
+        public string RemoveDuplicateLetter(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            HashSet<char> seen = new HashSet<char>();
+            Dictionary<int, int> lastOccurence = new Dictionary<int, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (lastOccurence.ContainsKey(c))
+                {
+                    lastOccurence[c] = i;
+                }
+                else
+                {
+                    lastOccurence.Add(c, i);
+                }
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (!seen.Contains(c))
+                {
+                    while (stack.Count > 0 && stack.Peek() > c && lastOccurence[stack.Peek()] > i)
+                    {
+                        seen.Remove(stack.Peek());
+                        stack.Pop();
+                    }
+                    stack.Push(c);
+                    seen.Add(c);
+                }
+            }
+            char[] result = new char[stack.Count];
+            while (stack.Count > 0)
+            {
+                result[stack.Count - 1] = stack.Peek();
+                stack.Pop();
+            }
+            return new String(result);
+        }
+    }
 }
