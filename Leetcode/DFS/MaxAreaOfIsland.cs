569ff07e4cba441e2dfcbf3af3d3236b193c8954
Novonil Das <das.novonil@gmail.com>
2021-04-28 00:10:52 -0500

    DFS

--- Leetcode/DFS/MaxAreaOfIsland.cs
+++ Leetcode/DFS/MaxAreaOfIsland.cs
@@ -42,5 +42,58 @@ namespace Leetcode.DFS
                 return false;
             return true;
         }
+
+
+
+        bool[][] visited;
+        public int MaxAreaOfIslandsTotal(int[][] grid)
+        {
+            if (grid == null || grid.Length == 0)
+                return 0;
+
+            int max = 0;
+            int rows = grid.Length;
+            int cols = grid[0].Length;
+
+            visited = new bool[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                visited[i] = new bool[cols];
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (!visited[i][j] && grid[i][j] == 1)
+                        max = Math.Max(max, dfss(grid, i, j));
+                }
+            }
+            return max;
+        }
+        public int dfss(int[][] grid, int i, int j)
+        {
+            if (!isValids(grid, i, j) || visited[i][j] || grid[i][j] == 0)
+                return 0;
+
+            visited[i][j] = true;
+
+            int up = dfss(grid, i - 1, j);
+            int down = dfss(grid, i + 1, j);
+            int left = dfss(grid, i, j - 1);
+            int right = dfss(grid, i, j + 1);
+
+            return 1 + left + right + up + down;
+        }
+
+        public bool isValids(int[][] grid, int i, int j)
+        {
+            int rows = grid.Length;
+            int cols = grid[0].Length;
+            if (i < 0 || i >= rows || j < 0 || j >= cols)
+                return false;
+            return true;
+        }
     }
 }
